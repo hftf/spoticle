751513b511331298955838b187b3e1f8149bf073
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-07-13 01:47:00 -0400

    Better __unicode__, added ordered_quizclips

--- spoticle/models.py
+++ spoticle/models.py
@@ -6,17 +6,24 @@ class Quiz(models.Model):
 	pub_date = models.DateTimeField('date published')
 
 	def __unicode__(self):
-		return self.name
+		return "%s (%s)" % (self.name, self.id)
+
+	def ordered_quizclips(self):
+		return self.quizclip_set.order_by("position")
 
 	class Meta:
 		verbose_name_plural = "Quizzes"
 
 class Clip(models.Model):
+	name = models.CharField(max_length=200)
+	path = models.CharField(max_length=200)
 	youtube_url = models.CharField(max_length=200)
 	start_seconds = models.IntegerField(default=0)
 	end_seconds = models.IntegerField()
 	duration = models.IntegerField()
-	path = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return "%s (%s)" % (self.name, self.id)
 
 class QuizClip(models.Model):
 	quiz = models.ForeignKey(Quiz)
@@ -25,4 +32,7 @@ class QuizClip(models.Model):
 	position = models.IntegerField()
 	question = models.CharField(max_length=200)
 	answers = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return "[%s] %s (%s)" % (self.position, self.quiz, self.clip)
 
