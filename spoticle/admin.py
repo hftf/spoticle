b7295281fdbb25446a64b7671481cb0307026c11
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-07-12 23:06:19 -0400

    Added admin.py, Quiz name and pub_date

---
+++ spoticle/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from spoticle.models import Quiz, Clip, QuizClip
+
+admin.site.register(Quiz)
--- spoticle/models.py
+++ spoticle/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 
 class Quiz(models.Model):
+	name = models.CharField(max_length=200)
 	path = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+	def __unicode__(self):
+		return self.name
 
 class Clip(models.Model):
 	youtube_url = models.CharField(max_length=200)
-	start_time = models.IntegerField(default=0)
-	end_time = models.IntegerField()
+	start_seconds = models.IntegerField(default=0)
+	end_seconds = models.IntegerField()
 	duration = models.IntegerField()
 	path = models.CharField(max_length=200)
 
 class QuizClip(models.Model):
 	quiz = models.ForeignKey(Quiz)
 	clip = models.ForeignKey(Clip)
+
 	position = models.IntegerField()
 	question = models.CharField(max_length=200)
 	answers = models.CharField(max_length=200)
+
