2b7c0215f165b1555166e6634af459af403b92aa
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-07-13 00:29:56 -0400

    Using render() instead

--- spoticle/views.py
+++ spoticle/views.py
@@ -1,15 +1,15 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+# from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from spoticle.models import Quiz
 
 def index(request):
 	quizzes = Quiz.objects.order_by('-pub_date')
-	template = loader.get_template('index.html')
-	context = RequestContext(request, {
+	context = {
 		'quizzes': quizzes,
-	})
-	return HttpResponse(template.render(context))
+	}
+	return render(request, 'index.html', context)
 
 def quiz(request, quiz_id):
 	return HttpResponse('You\'re looking at quiz %s' % quiz_id)
