af9d04bf0636233e3fc4261e056613f610bc92c3
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-07-12 22:40:44 -0400

    Fixed model order and field names

--- spoticle/models.py
+++ spoticle/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 
+class Quiz(models.Model):
+	path = models.CharField(max_length=200)
+
+class Clip(models.Model):
+	youtube_url = models.CharField(max_length=200)
+	start_time = models.IntegerField(default=0)
+	end_time = models.IntegerField()
+	duration = models.IntegerField()
+	path = models.CharField(max_length=200)
+
 class QuizClip(models.Model):
 	quiz = models.ForeignKey(Quiz)
 	clip = models.ForeignKey(Clip)
 	position = models.IntegerField()
 	question = models.CharField(max_length=200)
 	answers = models.CharField(max_length=200)
-
-class Quiz(models.Model):
-	path = models.CharField(max_length=200)
-
-class Clips(models.Model):
-	YTurl = models.CharField(max_length=200)
-	startTime = models.IntegerField(default=0)
-	endTime = models.IntegerField()
-	duration = models.IntegerField()
-	path = models.CharField(max_length=200)
-
-
