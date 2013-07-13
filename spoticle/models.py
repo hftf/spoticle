7db736ede9cdbec8748bccd20d3a03a03299bdd9
Parakh <jainparakh@gmail.com>
2013-07-12 22:37:04 -0400

    modified models.py

--- spoticle/models.py
+++ spoticle/models.py
@@ -6,3 +6,15 @@ class QuizClip(models.Model):
 	position = models.IntegerField()
 	question = models.CharField(max_length=200)
 	answers = models.CharField(max_length=200)
+
+class Quiz(models.Model):
+	path = models.CharField(max_length=200)
+
+class Clips(models.Model):
+	YTurl = models.CharField(max_length=200)
+	startTime = models.IntegerField(default=0)
+	endTime = models.IntegerField()
+	duration = models.IntegerField()
+	path = models.CharField(max_length=200)
+
+
