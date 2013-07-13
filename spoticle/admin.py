c9321a13da26748e6937d989ef0b9f89e5ca9075
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-07-13 03:20:49 -0400

    Added and qualified model fields

--- spoticle/admin.py
+++ spoticle/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from spoticle.models import Quiz, Clip, QuizClip
+from spoticle.models import Quiz, QuizAdmin, Clip, QuizClip
 
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Clip)
 admin.site.register(QuizClip)
--- spoticle/models.py
+++ spoticle/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import slugify
 
 class Quiz(models.Model):
 	name = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=40, unique=True) # generated
+	description = models.CharField(max_length=1000, blank=True)
+	require_in_order = models.BooleanField()
+	timer_seconds = models.TimeField()
+	tags = models.CharField(max_length=400, blank=True) # to make m2m
+
+	question = models.CharField(max_length=200, blank=True)
+	answers = models.CharField(max_length=200, blank=True)
+
 	path = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('date published') # or never
 
 	def __unicode__(self):
 		return "%s (%s)" % (self.name, self.id)
@@ -11,8 +22,19 @@ class Quiz(models.Model):
 	def ordered_quizclips(self):
 		return self.quizclip_set.order_by("position")
 
+	def save(self, *args, **kwargs):
+		"""
+		TODO: check if slug is a dupe
+		"""
+		if not self.slug:
+			self.slug = slugify(self.name)
+		super(Quiz, self).save()
+
 	class Meta:
 		verbose_name_plural = "Quizzes"
+
+class QuizAdmin(admin.ModelAdmin):
+	prepopulated_fields = { 'slug': ('name', ) }
 
 class Clip(models.Model):
 	name = models.CharField(max_length=200)
@@ -30,7 +52,7 @@ class QuizClip(models.Model):
 	clip = models.ForeignKey(Clip)
 
 	position = models.IntegerField()
-	question = models.CharField(max_length=200)
+	question = models.CharField(max_length=200, blank=True)
 	answers = models.CharField(max_length=200)
 
 	def __unicode__(self):
